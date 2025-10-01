d37afbfdfdd04541203b7bd2eeffe89b631ad67f
Darya Filipenko <aleksdarkgrimar@gmail.com>
2025-10-01 17:45:29 +0300

    загрузила 2 задание ко второму уроку

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -7,9 +7,29 @@
 * Для успешного прохождения тестов не меняйте названия переменных!
 */
 
-let dice1 // your code
-let dice2 // your code
-let isWinningDouble // your code
+let dice1                                              
+let dice2                                             
+let isWinningDouble = false                                    
+
+while (!isWinningDouble) {
+
+       dice1 = Math.floor(Math.random() * 6) + 1;
+       dice2 = Math.floor(Math.random() * 6) + 1;
+
+       console.log('Первый бросок: ' + dice1)
+       console.log('Второй бросок: ' + dice2)
+
+       if (dice1 === dice2 && dice1 > 3)
+        isWinningDouble = true
+
+if (isWinningDouble) {
+  console.log('Выигрышный дубль!')
+} else {
+  console.log('Не выигрышный дубль.')
+}
+       
+}
+
 
 /*
 console.log('Первый бросок: ' + dice1)
