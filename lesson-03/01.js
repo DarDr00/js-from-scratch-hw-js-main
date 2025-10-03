e09485dce2e373d0c280b8ae6e25f083149c748b
Darya Filipenko <aleksdarkgrimar@gmail.com>
2025-10-03 22:21:56 +0300

    загрузила все задания к третьему уроку

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,3 +1,11 @@
 // Напишите функцию calculateAverage, которая принимает 2 числа и возвращает их среднее арифметическое
 
-function calculateAverage() {}
+function calculateAverage(x, y) {
+   
+       average = Math.floor((x + y) / 2);
+       return average
+}
+
+let finalAverage = calculateAverage (6, 1005) 
+console.log (finalAverage)
+
--- lesson-03/02.js
+++ lesson-03/02.js
@@ -1,4 +1,18 @@
 // Разработайте функцию isEven, которая определяет, чётное ли число.
 // Функция должна возвращать true, если число чётное, и false — в противном случае
 
-function isEven() {}
+function isEven(integer) {
+    let checkInt = integer % 2;
+    if (checkInt === 0) {
+    return true
+    } else {
+    return false
+    }
+}
+
+let finalCheck = isEven (2)
+
+if (finalCheck === true) {
+console.log ('Число является чётным')
+} else {
+console.log ('Число нечетное')}
